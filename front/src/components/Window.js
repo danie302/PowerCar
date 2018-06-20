b18eb52e2c813c6c58b38bf56c0cfca9c2b4eeb1
Daniel <ddazaj@uninorte.edu.co>
2018-06-20 18:12:19 -0500

    Create window component

---
+++ front/src/components/Window.js
@@ -0,0 +1,14 @@
+//Dependencies
+import React, { Component } from 'react';
+
+class Window extends Component {
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Window;
