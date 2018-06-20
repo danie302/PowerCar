3d846af2596b8086ea8bfa7454ee55b8368bddcb
Daniel <ddazaj@uninorte.edu.co>
2018-06-20 14:59:22 -0500

    remove loginform

---
+++ front/src/components/LoginForm.js
@@ -0,0 +1,18 @@
+// Dependencies
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+// Assets
+import '../css/home.css';
+
+class LoginForm extends Component {
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
