66e5a3b9020f7e7014a1517b0efc4ea9264a71fa
Darrubla <darrubla@uninorte.edu.co>
2018-06-20 16:28:19 -0500

    navbar

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -4,30 +4,26 @@ import { Link } from 'react-router-dom';
 
 // Assets
 import '../css/home.css';
+import img1 from "../images/group-5@3x.png";
 
 class Home extends Component {
   render(){
     return(
-      <div className="color">
-        <div className="jumbotron jumbotron-fluid bg-dark">
-          <h1 className="center-title"> Bienvenido </h1>
-          <pre className="center">Plantilla de Logeo.</pre>
-        </div>
-        <div className="row">
-          <div className="card carta mr-auto ml-auto">
-            <div className="card-body text-center">
-              <h1 className="card-title">Inicia Sesion</h1>
-              <Link className="textcolor" to="/login">||||||||||||</Link>
-            </div>
-          </div>
-          <div className="card carta ml-auto mr-auto">
-            <div className="card-body  text-center">
-              <h1 className="card-title">Crear Cuenta</h1>
-              <Link className="textcolor" to="/register">||||||||||||</Link>
-            </div>
-          </div>
-        </div>
-      </div>
+
+
+  <nav className="navbar navbar-light bg-link">
+    <Link to= "/"className="navbar-brand" href="#">
+      <img src={ img1 } width="100" height="30" alt="">
+      </img>
+    </Link>
+    <form className="form-inline">
+      <button className="btn btn-link" type="button">Log in</button>
+      <button className="btn btn-success" type="button">Register</button>
+      </form>
+    </nav>
+
+
+
     );
   }
 }
